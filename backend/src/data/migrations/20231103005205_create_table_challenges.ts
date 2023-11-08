0797d52d2e7c0c53980d8996e42e5fd70ca20b70
Johanna Fransson <johannafransson@MacBook-Pro-som-tillhor-Johanna.local>
2023-11-08 12:04:50 -0300

    change to conditional drop if exists, add drop all tables command, refactor challenges and pools table for relation

--- backend/src/data/migrations/20231103005205_create_table_challenges.ts
+++ backend/src/data/migrations/20231103005205_create_table_challenges.ts
@@ -3,20 +3,18 @@ import { Knex } from "knex";
 
 export async function up(knex: Knex): Promise<void> {
     return knex.schema.createTable("challenges", (table) => {
-        table.increments();
-        table.uuid('poolId').defaultTo(null) //Not sure if it makes sense to have the foreign key as null?
+        table.uuid('id').primary().defaultTo(knex.fn.uuid());
         table.string("mintingContractAddress").nullable();
         table.integer("chainId").nullable();
         table.string("tokenId").nullable();
         table.string("imageUrl").nullable();
-        table.foreign("poolId").references("pools.id").onDelete("CASCADE");
         table.timestamps({ useCamelCase: true });
     });
 }
 
 
 export async function down(knex: Knex): Promise<void> {
-    return knex.schema.dropTable("challenges");
+    return knex.schema.dropTableIfExists("challenges");
 }
 
 
