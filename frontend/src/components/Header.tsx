040b07c436d30122a1faac51bd49744670404da1
Johanna Fransson <adrina0002@gmail.com>
2023-11-10 10:53:11 -0300

    header is done

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -21,24 +21,21 @@ const Header = ({ title, size, children }: HeaderProps) => {
     height: size === "big" || !size ? "25%" : "20%", //default to big size if no size specified
   };
   return (
-    <IonHeader className="ion-no-border custom-header" style={headerStyle}>
+    <IonHeader
+      className="ion-padding ion-no-border custom-header"
+      style={headerStyle}
+    >
       <IonToolbar>
-        {title ? (
-          <IonTitle className="ion-padding header-title">
-            My collective
-          </IonTitle>
-        ) : null}
+        {title ? <h5 className="header-title">My collective</h5> : null}
         {children}
 
         <IonButtons slot="end">
           <ConnectButton />
         </IonButtons>
       </IonToolbar>
-      <IonItem className="flexDirectionCol">
-        <IonTitle className="ion-padding-no-border page-header-title">
-          Discover
-        </IonTitle>
-      </IonItem>
+      <div className="header-title-container">
+        <h2 className="ion-padding-no-border page-header-title">Discover</h2>
+      </div>
     </IonHeader>
   );
 };
