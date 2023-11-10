66239bf9fc26367c8547dc9b1090bd8c1b1dfa02
Johanna Fransson <adrina0002@gmail.com>
2023-11-10 13:22:56 -0300

    add swipeCard in images component

--- frontend/src/components/Images/HorizontalSwipe.tsx
+++ frontend/src/components/Images/HorizontalSwipe.tsx
@@ -1,45 +1,38 @@
 import React, { useRef, useState } from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Pagination, Navigation } from "swiper/modules";
+import useGetChallenges from "@/hooks/Challenges/useGetChallenges";
+import SwipeCard from "./SwipeCard";
+import { PageLoadingIndicator } from "../PageLoadingIndicator";
 
 export default function HorizontalSwipe() {
+  const { challenges, loading } = useGetChallenges();
+
   return (
     <>
-      <Swiper
-        slidesPerView={3}
-        spaceBetween={10}
-        loop={true}
-        pagination={{
-          clickable: true,
-        }}
-        navigation={true}
-        modules={[Pagination, Navigation]}
-        className="mySwiper"
-      >
-        <div className="swiper-container">
-          <SwiperSlide>
-            <div className="card-img-swiper">
-              {" "}
-              <img
-                className="swiper-item-img"
-                alt="NFT Image"
-                /*    style={{ display: loading ? "none" : "block" }} */
-                src="https://ipfs.io/ipfs/bafybeihywodccwwggeoc5rxk3v3dbdwpqxyacku2zla5xf5i5hhoqpkafy"
-                /*        onLoad={() => setLoading(false)}
-                onError={() => setLoading(false)} */
-              />
-            </div>
-          </SwiperSlide>
-          <SwiperSlide>Slide 2</SwiperSlide>
-          <SwiperSlide>Slide 3</SwiperSlide>
-          <SwiperSlide>Slide 4</SwiperSlide>
-          <SwiperSlide>Slide 5</SwiperSlide>
-          <SwiperSlide>Slide 6</SwiperSlide>
-          <SwiperSlide>Slide 7</SwiperSlide>
-          <SwiperSlide>Slide 8</SwiperSlide>
-          <SwiperSlide>Slide 9</SwiperSlide>
-        </div>
-      </Swiper>
+      {loading || !challenges ? (
+        <PageLoadingIndicator />
+      ) : (
+        <Swiper
+          slidesPerView={3}
+          spaceBetween={10}
+          loop={true}
+          pagination={{
+            clickable: true,
+          }}
+          navigation={true}
+          modules={[Pagination, Navigation]}
+          className="mySwiper"
+        >
+          <div className="swiper-container">
+            {challenges.map((challenge: any, index: number) => (
+              <SwiperSlide key={index}>
+                <SwipeCard {...challenge} />
+              </SwiperSlide>
+            ))}
+          </div>
+        </Swiper>
+      )}
     </>
   );
 }
