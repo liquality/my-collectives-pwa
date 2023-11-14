1bcde08b19f93fe5685e0acfc61b68a9906a4cf0
Johanna Fransson <adrina0002@gmail.com>
2023-11-14 14:22:25 -0300

    add conditional rendering if connected or not

--- frontend/src/pages/Mint/NoGroups.tsx
+++ frontend/src/pages/Mint/NoGroups.tsx
@@ -1,11 +1,32 @@
-import { IonContent, IonItem, IonPage } from "@ionic/react";
+import { IonButton, IonContent, IonItem, IonPage } from "@ionic/react";
+import { useAccount } from "wagmi";
 
 export interface NoGroupsProps {}
 
 const NoGroups = ({}: NoGroupsProps) => {
+  const { address } = useAccount();
+
+  const handleCreateCollective = () => {
+    //TODO: handle routing and/or connect wallet here
+    //route.push(/createCollective)
+  };
+
   return (
     <IonContent>
-      <div className="purple-container"></div>
+      <div className="purple-container">
+        <p className="purple-container-text mb-1">
+          To find previous collected content and Collective connect the same
+          wallet.
+        </p>
+        <br></br>
+        <IonButton
+          onClick={handleCreateCollective}
+          shape="round"
+          color="primary"
+        >
+          {address ? "Create Collective" : "Connect"}
+        </IonButton>
+      </div>
     </IonContent>
   );
 };
