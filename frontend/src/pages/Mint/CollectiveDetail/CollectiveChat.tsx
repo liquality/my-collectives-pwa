c6b9bd08a2ffae338c6c8a0bd746cbebd5858eb1
Johanna Fransson <adrina0002@gmail.com>
2023-11-24 12:35:43 -0300

    add chat in chat tab

--- frontend/src/pages/Mint/CollectiveDetail/CollectiveChat.tsx
+++ frontend/src/pages/Mint/CollectiveDetail/CollectiveChat.tsx
@@ -3,8 +3,14 @@ import Header from "@/components/Header";
 import { RouteComponentProps } from "react-router";
 import CollectiveTopBar from "@/components/TopBars/CollectiveTopBar";
 import PageSearchBar from "@/components/PageSearchBar";
+import Chat from "@/components/Chat";
+import useGetGroupById from "@/hooks/Groups/useGetGroupById";
 
 const CollectiveChat: React.FC<RouteComponentProps> = (routerProps) => {
+  console.log(routerProps, "routerProps");
+  const { group, loading } = useGetGroupById(
+    "06623418-fc81-4cf0-8a1c-7e9ca41b64b6"
+  );
   return (
     <IonPage>
       <Header title="Collective Name" />
@@ -12,6 +18,8 @@ const CollectiveChat: React.FC<RouteComponentProps> = (routerProps) => {
         <CollectiveTopBar {...routerProps}>
           <PageSearchBar />
         </CollectiveTopBar>
+        {group && !loading ? <Chat group={group} /> : null}
+
         <p>Chaaaat component goes here</p>
       </IonContent>
     </IonPage>
