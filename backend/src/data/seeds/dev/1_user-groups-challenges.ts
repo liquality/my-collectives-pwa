d1b07b93c6ae19cebd7b4f15d92092651511d4a6
Johanna Fransson <adrina0002@gmail.com>
2023-11-28 16:39:17 -0300

    add new migrations for challenges

--- backend/src/data/seeds/dev/1_user-groups-challenges.ts
+++ backend/src/data/seeds/dev/1_user-groups-challenges.ts
@@ -10,7 +10,7 @@ export async function seed(knex: Knex): Promise<void> {
 
     const [{ id: userId }] = await trx("users").insert(
       {
-        publicAddress: "0x2De315AE6abAfE5ee6101DD13802736977E4f700",
+        publicAddress: "0xe7910F0b83ad155737043c771E2594f74B0BB739",
         nonce: AuthService.createNonce(),
       },
       ["id"]
@@ -31,46 +31,67 @@ export async function seed(knex: Knex): Promise<void> {
       admin: true,
     });
     console.log({ groupId });
-    // Inserts seed entries
 
 
     await trx("challenges").insert([
       {
-        mintingContractAddress: "0x5aa959de99e0e49b8a85e0a630a74a7b757772b7",
-        tokenId: 1,
-        chainId: 7777777,
-        imageUrl: ""
+        name: "Fried Potato Spectrum",
+        mintingContractAddress: "0xF41A3e3033D4e878943194B729AeC993a4Ea2045",
+        creatorOfMint: "creator.eth",
+        chainId: "777777",
+        tokenId: "16",
+        imageUrl: "ipfs://bafybeiewyx2fybhxaqgjrlsw5p2nj3w6hv3siifgy3ilprodxco5yq24gm",
+        category: "art",
+        platform: "zora",
+        expiration: "2024-07-02 13:23:12.322-0",
+        totalMints: "14",
+        expired: ""
 
       },
       {
-        mintingContractAddress: "0xf604c8204c4ca417635080c45f4ace8d0a7cf3ca",
-        tokenId: 7,
-        chainId: 7777777,
-        imageUrl: ""
+        name: "ENERGY",
+        mintingContractAddress: "0xF41A3e3033D4e878943194B729AeC993a4Ea2045",
+        creatorOfMint: "",
+        chainId: "777777",
+        tokenId: "1",
+        imageUrl: "ipfs://bafybeifydhtmthjtja7y4m6eyzmyuvzgbt4w7ejuldivv5iitwqqsihxfu",
+        category: "art",
+        platform: "zora",
+        expiration: "2024-07-04 13:58:18.943-0",
+        totalMints: "3",
+
       },
+
       {
-        mintingContractAddress: "0xa51e30df685c413d4b399ea2fd0d90373b01d0b4",
-        tokenId: 1,
-        chainId: 7777777,
-        imageUrl: ""
+        name: "Gitcoin Impact Report 01: PDF Onchain",
+        mintingContractAddress: "0x81D226fb36cA785583E79E84312335d0e166D59B",
+        creatorOfMint: "gitcoin.eth",
+        chainId: "777777",
+        tokenId: "1",
+        imageUrl: "ipfs://bafybeieopdawoakmvioeyutkgogc34npcxwzuv2eo3pyconpkbs2ygobtu",
+        category: "art",
+        platform: "zora",
+        expiration: "2030-06-06 13:59:08.053-03",
+        totalMints: "2",
+
+
       },
+
       {
-        mintingContractAddress: "0x6c10be7531ec182b90fa65679110ba2b3dcefb8c",
-        tokenId: 1,
-        chainId: 7777777,
+        name: "Sonic Zorb",
+        mintingContractAddress: "0xb3d0bA3c295FdB0918Fe4BcDE04f62f36E60F50c",
+        creatorOfMint: "amy.eth",
+        chainId: "777777",
+        tokenId: "0",
+        imageUrl: "ipfs://QmTPUXc8Kh2NEA6M4B4mKDqxs5dRK5oVuwCj8iyBFtyHcW/0.png",
+        category: "art",
+        platform: "zora",
+        expiration: "2024-07-01 13:59:49.831-03",
+        totalMints: "13",
+
+
       },
-      {
-        mintingContractAddress: "0xf032d92a685fbcd4420e437b7903ed866be04b87",
-        tokenId: 1,
-        chainId: 7777777,
-        imageUrl: ""
-      },
-      {
-        mintingContractAddress: "0x88ad6d028f415ae1ee36c98466fe5d1e7f33c327",
-        tokenId: 1,
-        chainId: 7777777,
-        imageUrl: ""
-      },
+
     ]);
   });
 }
--- frontend/src/components/GenerateInvite.tsx
+++ frontend/src/components/GenerateInvite.tsx
@@ -1,4 +1,6 @@
+import { useSignInWallet } from "@/hooks/useSignInWallet";
 import useToast from "@/hooks/useToast";
+import ApiService from "@/services/ApiService";
 import InvitesService from "@/services/Invites";
 import { Group } from "@/types/chat";
 import { IonText } from "@ionic/react";
@@ -10,12 +12,11 @@ interface InviteProps {
 }
 const GenerateInviteBtn = (props: InviteProps) => {
   const { groupId, setInviteLink, inviteLink } = props;
+  const { user } = useSignInWallet();
   const handleGenerateInvite = async () => {
     //presentToast(`You generated a invite link, click to copy!`);
     //TODO: here it should give you the first invite to copy from userId and groupId
-    const result = await InvitesService.createInvite({
-      group_id: groupId,
-    } as Partial<Group>);
+    const result = await ApiService.getInvite(groupId, user.id);
     console.log(result, "wats result?");
     setInviteLink(result.invite_link);
   };
