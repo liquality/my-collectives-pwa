5ec898c2138d45b22fba06b5cde0d9b4bfd7b51a
Johanna Fransson <adrina0002@gmail.com>
2023-12-06 16:37:18 -0300

    add unique constraint for public Address

--- backend/src/data/migrations/20231103005059_create_table_groups.ts
+++ backend/src/data/migrations/20231103005059_create_table_groups.ts
@@ -6,7 +6,7 @@ export async function up(knex: Knex): Promise<void> {
         table.uuid('id').primary().defaultTo(knex.fn.uuid());
         table.string('name').notNullable();
         table.string('description').nullable();
-        table.string('publicAddress').nullable();
+        table.string('publicAddress').nullable().unique();
         table.integer('mintCount').defaultTo(0).notNullable(); //increment this value after someone has minted to/thorugh group 
         table.integer('wonChallenges').defaultTo(0).notNullable(); //increment this value after the group has been set as Top Contributor
         table.integer('totalRewardedInEth').defaultTo(0).notNullable(); //NOT SURE BUT: increment this value after listening to withdrawalReward()→ emits an event (participant: userWhoGetTheReward, amount), this is related to a poolAddress and that pool has an assosciated groupAddress that I can get?
