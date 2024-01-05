f75ca161ce184e238ef1d93baea9538149e552f0
Bradley Suira <bradleysuira@gmail.com>
2024-01-05 14:51:11 -0500

    feat: added participation filter for pool

--- backend/src/services/sync/rewards.ts
+++ backend/src/services/sync/rewards.ts
@@ -1,39 +1,51 @@
 import { ethers } from "ethers";
 import { dbClient } from "../../data";
-import { convertToDate, fetchReservoirData, getTokenMetadataFromZora } from "../../utils";
+import {
+  convertToDate,
+  fetchReservoirData,
+  getTokenMetadataFromZora,
+} from "../../utils";
 import * as MyCollectives from "@koderholic/my-collectives";
 import { Config } from "@koderholic/my-collectives";
 
 export class SyncRewardsService {
-    constructor() {
-        MyCollectives.setConfig({} as Config);
+  constructor() {
+    MyCollectives.setConfig({} as Config);
+  }
+
+  async getRewardsData(memberAddress: string, pools: string[]) {
+    let result: any[] = [];
+    for (const pool of pools) {
+      const participationResponse = await MyCollectives.Pool.getParticipation(
+        pool,
+        memberAddress
+      );
+
+      if (participationResponse && participationResponse.id != "") {
+        const participation = {
+          address: participationResponse.id,
+          contribution: participationResponse.contribution?.toString(),
+          rewardAmount: ethers.utils.formatEther(
+            participationResponse.rewardedAmount
+          ),
+          rewardAvailable: ethers.utils.formatEther(
+            participationResponse.rewardAvailable
+          ),
+        };
+
+        const totalContributions =
+          await MyCollectives.Pool.getTotalContributions(pool);
+        const rewards = await MyCollectives.Pool.getPoolReward(pool);
+
+        result.push({
+          pool,
+          participation,
+          totalContributions,
+          rewards,
+        });
+      }
     }
 
-    async getRewardsData(memberAddress: string, pools: string[]) {
-        let result: any[] = [];
-        for (const pool of pools) {
-          const participationResponse = await MyCollectives.Pool.getParticipation(
-            pool,
-            memberAddress
-          );
-
-          const participation = {
-            address: participationResponse[0], 
-            contribution: participationResponse[1].toString(), 
-            rewardAmount: ethers.utils.formatEther(participationResponse[2])
-        };
-          const totalContributions = await MyCollectives.Pool.getTotalContributions(pool);
-          const rewards = await MyCollectives.Pool.getPoolReward(pool);
-          
-          result.push({
-            pool,
-            participation,
-            totalContributions,
-            rewards,
-          });
-        }
-    
-        return result;
-      },
-   
+    return result;
+  }
 }
