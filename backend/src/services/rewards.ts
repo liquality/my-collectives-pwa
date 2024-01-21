46321c3eb7a579b689d41bc6df55cd574e399905
Bradley Suira <bradleysuira@gmail.com>
2024-01-21 15:51:56 -0500

    fix: updated delete/insert for user rewards and drop non used public address

--- backend/src/services/rewards.ts
+++ backend/src/services/rewards.ts
@@ -4,11 +4,10 @@ import { PoolsService } from "./pools";
 import { getTopContributorFromEvents } from "../utils/events-query/top-contributor-zora";
 import * as MyCollectives from "@liquality/my-collectives-sdk";
 
-
 export class RewardsService {
   constructor() {
     MyCollectives.setConfig({
-      RPC_URL: process.env.RPC_URL || '',
+      RPC_URL: process.env.RPC_URL || "",
       PIMLICO_API_KEY: process.env.PIMLICO_API_KEY,
       BICONOMY_PAYMASTER: process.env.BICONOMY_PAYMASTER,
       BICONOMY_BUNDLER_API_KEY: process.env.BICONOMY_BUNDLER_API_KEY,
@@ -17,15 +16,16 @@ export class RewardsService {
   }
 
   async getPoolParticipation(memberAddress: string, poolsAddress: string) {
-    console.log(poolsAddress, 'poolsaddress?', memberAddress)
+    console.log(poolsAddress, "poolsaddress?", memberAddress);
     const participationResponse = await MyCollectives.Pool.getParticipation(
       poolsAddress,
       memberAddress
     );
 
-    console.log(participationResponse, 'participation respons')
-
-    if (participationResponse && participationResponse.participant != "") {
+    if (
+      participationResponse?.participant.toLowerCase() ===
+      memberAddress.toLowerCase()
+    ) {
       const participation = {
         address: participationResponse.participant,
         contribution: participationResponse.contribution?.toString(),
@@ -34,6 +34,7 @@ export class RewardsService {
           participationResponse.rewardAvailable
         ),
       };
+      console.log(participation, "participation response");
       return participation;
     }
 
@@ -41,7 +42,9 @@ export class RewardsService {
   }
 
   async getTotalContributions(poolsAddress: string) {
-    const totalContributions = await MyCollectives.Pool.getTotalContributions(poolsAddress);
+    const totalContributions = await MyCollectives.Pool.getTotalContributions(
+      poolsAddress
+    );
 
     return totalContributions;
   }
@@ -73,70 +76,73 @@ export class RewardsService {
       .from("pools")
       .join("challenges", "pools.challengeId", "=", "challenges.id")
       .where(dbClient.raw("challenges.expiration > ?", [dbClient.fn.now()]));
-    
+
     const userRewards: any[] = [];
     for (const pool of pools) {
-      /*      console.log(user.publicAddress,
-             pool.publicAddress, 'pools address') */
       const poolParticipation = await this.getPoolParticipation(
         user.publicAddress,
         pool.publicAddress
       );
 
-      console.log(poolParticipation, 'pool participation')
+      console.log(poolParticipation, "pool participation");
       if (poolParticipation) {
         userRewards.push({
           numberOfMints: poolParticipation.contribution,
           amountInEthEarned: pool.rewardAmount,
           userId: user.id,
-          publicAddress: user.publicAddress,
           poolId: pool.poolId,
           groupId: pool.groupId,
         });
       }
-
-      //console.log(userRewards, 'userrewards array')
     }
 
     try {
-      /*    const result = await dbClient.raw(
-           `? ON CONFLICT ("publicAddress", "poolId", "groupId")
-                 DO NOTHING
-                 RETURNING *;`,
-           [dbClient("user_rewards").insert(userRewards)]
-         );
-    */
-      try {
-//        it will need a privatekey and throws Error: invalid mnemonic
-
-        //const hej = await RewardsService.setTopContributorGroup()
-
-      } catch (error) {
-        console.log(error, 'error getting top contri')
-      }
-
-      const result = dbClient("user_rewards").insert(userRewards)
-      //console.debug("updated user_rewards:", result);
+      await dbClient.transaction(async (trx) => {
+        // remove existing data
+        await trx("user_rewards")
+        .whereIn('userId', userRewards.map(i => i.userId))
+        .whereIn('poolId', userRewards.map(i => i.poolId))
+        .whereIn('groupId', userRewards.map(i => i.groupId))
+        .del();
+        //insert new data
+        const result = await trx("user_rewards").insert(userRewards);
+        console.debug("user_rewards result:", result);
+        return { success: true };
+      });
     } catch (error) {
-      console.log(error, "user_rewards error");
+      console.error("user_rewards error:", error);
+      return { success: false };
+    }
+
+    try {
+      const topContripResult = await RewardsService.setTopContributorGroup();
+      console.debug("setTopContributorGroup:", topContripResult);
+    } catch (error) {
+      console.log(error, "setTopContributorGroup error");
       return { success: false };
     }
   }
 
   public static async setTopContributorGroup(): Promise<any> {
     let mnemonic = process.env.AUTHORIZED_OPERATOR_PHRASE || "YOUR MNEMONIC";
-    let privateKey = ethers.Wallet.fromMnemonic(mnemonic).privateKey
-    console.log(privateKey, 'privateKey')
+    let privateKey = ethers.Wallet.fromMnemonic(mnemonic).privateKey;
+    console.log(privateKey, "privateKey");
     try {
       //1)Get all pools  that are expired
-      const expiredPools = await PoolsService.findAllPoolsThatAreExpired()
-      console.log(expiredPools, 'expired pools', expiredPools.length)
+      const expiredPools = await PoolsService.findAllPoolsThatAreExpired();
+      console.log(expiredPools, "expired pools", expiredPools.length);
 
       const poolsToSetTopContributor: string[] = [];
       for (const pool of expiredPools) {
-        //2) Check if topContributor has already been set 
-        const topContributor = await MyCollectives.HoneyPot.getTopContributor(pool.honeyPotAddress)
-        console.log(topContributor, 'HAS TOP CONTRIBUTOR BEEN SET?', pool.honeyPotAddress)
+        //2) Check if topContributor has already been set
+        const topContributor = await MyCollectives.HoneyPot.getTopContributor(
+          pool.honeyPotAddress
+        );
+        console.log(
+          topContributor,
+          "HAS TOP CONTRIBUTOR BEEN SET?",
+          pool.honeyPotAddress
+        );
         // 3) If the top contributor is not set, add the pool to the new array
         if (topContributor !== ethers.constants.AddressZero) {
           poolsToSetTopContributor.push(pool);
@@ -146,27 +152,53 @@ export class RewardsService {
                   const distributeRewardsResponse = await MyCollectives.Pool.distributeRewards(privateKey, pool.publicAddress)
                   console.log(distributeRewardsResponse, 'distribute rewards response111') */
         }
-        console.log(pool.honeyPotAddress, 'honey pot address from pool')
+        console.log(pool.honeyPotAddress, "honey pot address from pool");
         //6) Scrape events from ethers, create a leaderboard and return top contributor
-        const topContributorAddress = await getTopContributorFromEvents(pool.createdAt, pool.expiration, pool.mintingContractAddress, pool.network)
+        const topContributorAddress = await getTopContributorFromEvents(
+          pool.createdAt,
+          pool.expiration,
+          pool.mintingContractAddress,
+          pool.network
+        );
         if (topContributorAddress?.address) {
-          const setTopContributorResponse = await MyCollectives.HoneyPot.setTopContributor(privateKey, pool.honeyPotAddress, topContributorAddress.address)
-          console.log(setTopContributorResponse, 'wat is response TOP CONTRIBUTOR')
+          const setTopContributorResponse =
+            await MyCollectives.HoneyPot.setTopContributor(
+              privateKey,
+              pool.honeyPotAddress,
+              topContributorAddress.address
+            );
+          console.log(
+            setTopContributorResponse,
+            "wat is response TOP CONTRIBUTOR"
+          );
           if (setTopContributorResponse.txHash) {
             //TODO: find collective by topcontributor.address, if it exists, send the reward
-            console.log(privateKey, 'honeypot:', pool.honeyPotAddress, 'publicaddress:', pool.publicAddress,)
-            const sendRewardsResponse = await MyCollectives.HoneyPot.sendReward(privateKey, pool.honeyPotAddress)
-            console.log(sendRewardsResponse, 'send rewards response')
-            const distributeRewardsResponse = await MyCollectives.Pool.distributeRewards(privateKey, pool.publicAddress)
-            console.log(distributeRewardsResponse, 'distribute rewards response')
-
+            console.log(
+              privateKey,
+              "honeypot:",
+              pool.honeyPotAddress,
+              "publicaddress:",
+              pool.publicAddress
+            );
+            const sendRewardsResponse = await MyCollectives.HoneyPot.sendReward(
+              privateKey,
+              pool.honeyPotAddress
+            );
+            console.log(sendRewardsResponse, "send rewards response");
+            const distributeRewardsResponse =
+              await MyCollectives.Pool.distributeRewards(
+                privateKey,
+                pool.publicAddress
+              );
+            console.log(
+              distributeRewardsResponse,
+              "distribute rewards response"
+            );
           }
         }
-
       }
     } catch (error) {
-      console.log(error, 'error in top contributor')
+      console.log(error, "error in top contributor");
     }
-
   }
 }
